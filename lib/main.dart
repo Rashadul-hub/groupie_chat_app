0a7acafaba769a46ed9909cfb81c746d0e6b16b7
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-03-08 23:41:32 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
+import 'package:groupie/pages/auth/home_page.dart';
+import 'package:groupie/pages/auth/login_page.dart';
 import 'package:groupie/shared/constants.dart';
+
+import 'helper/helper_function.dart';
 
 void main()async{
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,11 +27,36 @@ void main()async{
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _isSignedIn = false;
+
+  @override
+  void initState() {
+    super.initState();
+    getUserLoggedInStatus();
+  }
+
+  getUserLoggedInStatus() async{
+    await HelperFunctions.getUserLoggedInStatus().then((value){
+      if (value != null){
+        _isSignedIn = value;
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: _isSignedIn ? const HomePage() : const LoginPage() ,
+
+    );
   }
 }
