080e15b29be0705621e8723e8b399433d3312fbb
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-03-09 00:29:19 +0600

    Update

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,6 +1,9 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
 class Constants{
   static String  appId = "1:614124791265:web:2cb9ccbe7e4095ffd69ea0";
   static String  apiKey = "AIzaSyBoikpIk54LaY_Nl3rfFw0zHulrHLMl5ZY";
   static String  messagingSenderId = "614124791265";
   static String  projectId = "chatappflutter-80310";
+  final primaryColor = const Color(0xFFee7b64);
 }
