f4f7587c41a9101cc5b6c60431668756de584bff
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-03-26 16:20:38 +0600

    Created AuthService for Firebase Authentication
    Registration with Email & password ,
    SignOut

---
+++ lib/service/auth_service.dart
@@ -0,0 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:groupie/helper/helper_function.dart';
+import 'package:groupie/service/database_service.dart';
+
+class AuthService {
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+
+  //login
+
+  //register
+  Future registerUserWithEmailandPassword(
+      String fullName, String email, String password) async {
+    try {
+      User user = (await firebaseAuth.createUserWithEmailAndPassword(
+              email: email, password: password))
+          .user!;
+
+      if (user != null) {
+        // call our database service to update the user data.
+        await DatasbaseService(uid: user.uid).updateUserData(fullName, email);
+        return true;
+      }
+    } on FirebaseAuthException catch (e) {
+      return e.message;
+    }
+  }
+
+  //sign out
+Future signout() async{
+    try{
+      await HelperFunctions.saveUserLoggedInStatus(false);
+      await HelperFunctions.saveUserEmailSF("");
+      await HelperFunctions.saveUserNameSF("");
+      await firebaseAuth.signOut();
+
+
+    }catch(e){
+
+    }
+}
+}
