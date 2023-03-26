8778c3922adcb0533aa0e22964930273df0c82c0
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-03-26 16:15:57 +0600

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,9 @@ class _MyAppState extends State<MyApp> {
   getUserLoggedInStatus() async{
     await HelperFunctions.getUserLoggedInStatus().then((value){
       if (value != null){
-        _isSignedIn = value;
+        setState(() {
+          _isSignedIn = value;
+        });
       }
     });
   }
