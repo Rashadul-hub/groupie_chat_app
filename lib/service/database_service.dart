9b8ac86c6d765e2fdbed76bb65ba3f04397067e9
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-03-26 16:21:21 +0600

    Database Service for Reference & Update userdata

---
+++ lib/service/database_service.dart
@@ -0,0 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class DatasbaseService {
+  final String? uid;
+  DatasbaseService({this.uid});
+
+  // reference for our collections
+  final CollectionReference userCollection =
+      FirebaseFirestore.instance.collection("users");
+  final CollectionReference groupCollection =
+      FirebaseFirestore.instance.collection("groups");
+
+  // updating the userdata
+  Future updateUserData(String fullName, String email) async {
+    return await userCollection.doc(uid).set({
+      "fullName": fullName,
+      "email": email,
+      "groups": [],
+      "profilePic": "",
+      "uid": uid,
+    });
+  }
+}
