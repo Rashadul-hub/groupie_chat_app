00bac4748e8d520c05a0d8a7c4feb746b72b9899
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-04-19 02:37:50 +0600

    Data Fetch From FireBase
    For Email & User Name information

--- lib/pages/auth/profile_page.dart
+++ lib/pages/auth/profile_page.dart
@@ -6,36 +6,37 @@ import '../../widgets/widgets.dart';
 import 'login_page.dart';
 
 class ProfilePage extends StatefulWidget {
-
   String userName;
   String email;
 
-   ProfilePage({Key? key, required this.userName, required this.email}) : super(key: key);
+  ProfilePage({Key? key, required this.userName, required this.email})
+      : super(key: key);
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  AuthService authService =  AuthService();
+  AuthService authService = AuthService();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-         appBar: AppBar(
-           backgroundColor: Theme.of(context).primaryColor ,
-           elevation: 0,
-           centerTitle: true,
-           title: const Text(
-             "Profile",
-             style: TextStyle(
-               color: Colors.white,
-               fontSize: 27,
-               fontWeight: FontWeight.bold,
-             ),
-           ),
-         ),
-      drawer: Drawer(child: ListView(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text(
+          "Profile",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 27,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      drawer: Drawer(
+          child: ListView(
         padding: const EdgeInsets.symmetric(vertical: 50),
         children: <Widget>[
           Icon(
@@ -61,7 +62,6 @@ class _ProfilePageState extends State<ProfilePage> {
             onTap: () {
               nextScreen(context, const HomePage());
             },
-
             contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
             leading: const Icon(Icons.group),
             title: const Text(
@@ -105,7 +105,7 @@ class _ProfilePageState extends State<ProfilePage> {
                             Navigator.of(context).pushAndRemoveUntil(
                                 MaterialPageRoute(
                                     builder: (context) => const LoginPage()),
-                                    (route) => false);
+                                (route) => false);
                           },
                           icon: const Icon(
                             Icons.done,
@@ -116,12 +116,6 @@ class _ProfilePageState extends State<ProfilePage> {
                     );
                   });
             },
-
-            //   authService.signout().whenComplete(() {
-            //     nextScreenReplace(context, const LoginPage());
-            //   });
-            // },
-
             contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
             leading: const Icon(Icons.exit_to_app),
             title: const Text(
@@ -130,10 +124,36 @@ class _ProfilePageState extends State<ProfilePage> {
             ),
           )
         ],
-      )
+      )),
+      body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 170),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Icon(
+              Icons.account_circle,
+              size: 200,
+              color: Colors.grey[700],
+            ),
+            const SizedBox(height: 15,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const  Text("Full Name", style: TextStyle(fontSize: 17),),
+                  Text(widget.userName, style: const TextStyle(fontSize: 17),),
+              ],
+            ),
+            Divider(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const  Text("Email", style: TextStyle(fontSize: 17),),
+                Text(widget.email, style: const TextStyle(fontSize: 17),),
+              ],
+            ),
+          ],
+        ),
       ),
-
-      
     );
   }
 }
