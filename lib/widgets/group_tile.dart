cdbf81fc81f81c6fcb211b16023917d64f7c39fc
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-04-19 17:46:05 +0600

    Create Group tile for Group List

---
+++ lib/widgets/group_tile.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class GroupTile extends StatefulWidget {
+  final String userName;
+  final String groupId;
+  final String groupName;
+
+  const GroupTile(
+      {Key? key,
+      required this.groupName,
+      required this.userName,
+      required this.groupId})
+      : super(key: key);
+
+  @override
+  State<GroupTile> createState() => _GroupTileState();
+}
+
+class _GroupTileState extends State<GroupTile> {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(widget.groupId),
+      subtitle: Text(widget.groupName),
+
+    );
+  }
+}
