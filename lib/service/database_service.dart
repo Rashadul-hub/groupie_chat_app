09d713ac87e060ca70556333a78a43e117d5be67
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-04-26 03:03:00 +0600

    Toggling the group Join/Exit

--- lib/service/database_service.dart
+++ lib/service/database_service.dart
@@ -102,5 +102,31 @@ class DatasbaseService {
     }
   }
 
+  //toggling the group join/exit
+  Future toggleGroupJoin(String groupId, String userName,
+      String groupName) async {
+    //doc reference
+    DocumentReference userDocumentReference = userCollection.doc(uid);
+    DocumentReference groupDocumentReference = groupCollection.doc(groupId);
 
+    DocumentSnapshot documentSnapshot = await userDocumentReference.get();
+    List<dynamic> groups = await documentSnapshot['groups'];
+
+    // if user has our groups -> then remove then or also in other part re join
+    if (groups.contains("${groupId}_$groupName")) {
+      await userDocumentReference.update({
+        "groups": FieldValue.arrayRemove(["${groupId}_$groupName"])
+      });
+      await groupDocumentReference.update({
+        "members": FieldValue.arrayRemove(["${uid}_$userName"])
+      });
+    } else {
+        await userDocumentReference.update({
+          "groups": FieldValue.arrayUnion(["${groupId}_$groupName"])
+        });
+        await groupDocumentReference.update({
+          "members": FieldValue.arrayUnion(["${uid}_$userName"])
+        });
+      }
+    }
 }
