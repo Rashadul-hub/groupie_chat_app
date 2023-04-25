3d12dfdc3acbda360f4495b59d059aefbd0fbbd7
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-04-26 03:03:56 +0600

    Updated Search Bar & Joining Group

--- lib/pages/auth/search_page.dart
+++ lib/pages/auth/search_page.dart
@@ -1,4 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:groupie/helper/helper_function.dart';
+import 'package:groupie/pages/auth/chat_page.dart';
+import 'package:groupie/service/database_service.dart';
+import 'package:groupie/widgets/widgets.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -8,11 +14,216 @@ class SearchPage extends StatefulWidget {
 }
 
 class _SearchPageState extends State<SearchPage> {
+  TextEditingController searchController = TextEditingController();
+  bool _isLoading = false;
+  QuerySnapshot? searchSnapshot;
+  bool hasUserSearched = false;
+  String userName = "";
+  bool isJoined = false;
+  User? user;
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUserIdAndName();
+  }
+
+  getCurrentUserIdAndName() async {
+    await HelperFunctions.getUserNameFromSF().then((value) {
+      setState(() {
+        userName = value!;
+      });
+    });
+    user = FirebaseAuth.instance.currentUser;
+  }
+
+  //String Manipulation
+  String getId(String res) {
+    return res.substring(0, res.indexOf("_"));
+  }
+
+  String getName(String r) {
+    return r.substring(r.indexOf("_") + 1);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return  const Scaffold(
-      body: Center(
-        child: Text("SearchPage"),
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        elevation: 0,
+        backgroundColor: Theme.of(context).primaryColor,
+        title: const Text(
+          "Search",
+          style: TextStyle(
+              fontSize: 27, fontWeight: FontWeight.bold, color: Colors.white),
+        ),
+      ),
+      body: Column(
+        children: [
+          Container(
+            color: Theme.of(context).primaryColor,
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: searchController,
+                    style: const TextStyle(color: Colors.white),
+                    decoration: const InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Search Groups . . . .",
+                        hintStyle:
+                            TextStyle(color: Colors.white, fontSize: 16)),
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {
+                    initiateSearchMethod();
+                  },
+                  child: Container(
+                    width: 40,
+                    height: 40,
+                    decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    child: const Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          _isLoading
+              ? Center(
+                  child: CircularProgressIndicator(
+                    color: Theme.of(context).primaryColor,
+                  ),
+                )
+              : groupList(),
+        ],
+      ),
+    );
+  }
+
+  initiateSearchMethod() async {
+    if (searchController.text.isNotEmpty) {
+      setState(() {});
+      await DatasbaseService()
+          .searchByName(searchController.text)
+          .then((snapshot) {
+        setState(() {
+          searchSnapshot = snapshot;
+          _isLoading = false;
+          hasUserSearched = true;
+        });
+      });
+    }
+  }
+
+  groupList() {
+    return hasUserSearched
+        ? ListView.builder(
+            shrinkWrap: true,
+            itemCount: searchSnapshot!.docs.length,
+            itemBuilder: (context, index) {
+              return groupTile(
+                userName,
+                searchSnapshot!.docs[index]['groupId'],
+                searchSnapshot!.docs[index]['groupName'],
+                searchSnapshot!.docs[index]['admin'],
+              );
+            },
+          )
+        : Container();
+  }
+
+  //Function to check whether user already exists in group !!
+  joinedOrNot(
+      String userName, String groupId, String groupName, String admin) async {
+    await DatasbaseService(uid: user!.uid)
+        .isUserJoined(groupName, groupId, userName)
+        .then((value) {
+      setState(() {
+        isJoined = value;
+      });
+    });
+  }
+
+  Widget groupTile(
+      String userName, String groupId, String groupName, String admin) {
+    // Function to check whether user already exists in group !!
+    joinedOrNot(userName, groupId, groupName, admin);
+
+    return ListTile(
+      contentPadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      leading: CircleAvatar(
+        radius: 30,
+        backgroundColor: Theme.of(context).primaryColor,
+        child: Text(
+          groupName.substring(0, 1).toUpperCase(),
+          style: const TextStyle(color: Colors.white),
+        ),
+      ),
+      title: Text(
+        groupName,
+        style: const TextStyle(fontWeight: FontWeight.w600),
+      ),
+      subtitle: Text("Admin: ${getName(admin)}"),
+      trailing: InkWell(
+        onTap: () async {
+          await DatasbaseService(uid: user!.uid)
+              .toggleGroupJoin(groupId, userName, groupName);
+          if (isJoined) {
+            setState(() {
+              isJoined = !isJoined;
+            });
+            showSnackBar(context, Colors.green, "Successfully Joined The Group");
+            Future.delayed(const Duration(seconds: 2), ()  {
+              nextScreen(
+                  context,
+                  ChatPage(
+                      groupId: groupId,
+                      groupName: groupName,
+                      userName: userName));
+            });
+          }
+          else {
+            setState(() {
+              isJoined = !isJoined;
+              showSnackBar(context, Colors.red, "Left The Group $groupName");
+            });
+          }
+        },
+        child: isJoined
+            ? Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.black,
+                  border: Border.all(color: Colors.white, width: 1),
+                ),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: const Text(
+                  "Joined",
+                  style: TextStyle(color: Colors.white),
+                ),
+              )
+            : Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Theme.of(context).primaryColor,
+                ),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: const Text(
+                  "Join Now",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
       ),
     );
   }
