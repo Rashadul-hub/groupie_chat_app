a2c44a09833c6078bee12272baf93283132a4253
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-04-26 04:27:47 +0600

    Finalizing with Messages Tile For Getting the Texts

--- lib/widgets/message_tile.dart
+++ lib/widgets/message_tile.dart
@@ -19,6 +19,58 @@ class MessageTile extends StatefulWidget {
 class _MessageTileState extends State<MessageTile> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.only(
+          top: 4,
+          bottom: 4,
+          left: widget.sentByMe ? 0 : 24,
+          right: widget.sentByMe ? 24 : 0),
+      alignment: widget.sentByMe ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin: widget.sentByMe
+            ? const EdgeInsets.only(left: 30)
+            : const EdgeInsets.only(right: 30),
+        padding:
+            const EdgeInsets.only(top: 17, bottom: 17, left: 20, right: 20),
+        decoration: BoxDecoration(
+            borderRadius: widget.sentByMe
+                ? const BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                    bottomLeft: Radius.circular(20),
+                  )
+                : const BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                    bottomRight: Radius.circular(20),
+                  ),
+            color: widget.sentByMe
+                ? Theme.of(context).primaryColor
+                : Color(0xffaeb9cc)),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              widget.sender.toUpperCase(),
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+                letterSpacing: -0.5,
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Text(
+              widget.message,
+              textAlign: TextAlign.justify,
+              style: const TextStyle(fontSize: 17, color: Colors.white),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
