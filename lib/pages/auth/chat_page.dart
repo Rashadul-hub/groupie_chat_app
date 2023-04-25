923f6d656f43061a2ceac4ad0a729d9d4227df4a
Rashadul Islam Himu <rashadulislamhimu@gmail.com>
2023-04-26 03:43:44 +0600

    Designed Chat Page For Texting

--- lib/pages/auth/chat_page.dart
+++ lib/pages/auth/chat_page.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:groupie/pages/auth/group_info.dart';
 import 'package:groupie/service/database_service.dart';
+import 'package:groupie/widgets/message_tile.dart';
 import 'package:groupie/widgets/widgets.dart';
 
 class ChatPage extends StatefulWidget {
@@ -21,8 +22,8 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
-
   Stream<QuerySnapshot>? chats;
+  TextEditingController messageController = TextEditingController();
   String admin = "";
 
   @override
@@ -31,20 +32,18 @@ class _ChatPageState extends State<ChatPage> {
     super.initState();
   }
 
-  getChatandAdmin(){
-    DatasbaseService().getChats(widget.groupId).then((val){
+  getChatandAdmin() {
+    DatasbaseService().getChats(widget.groupId).then((val) {
       setState(() {
         chats = val;
       });
     });
-    DatasbaseService().getGroupAdmin(widget.groupId).then((val){
+    DatasbaseService().getGroupAdmin(widget.groupId).then((val) {
       setState(() {
         admin = val;
       });
     });
-
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -57,15 +56,99 @@ class _ChatPageState extends State<ChatPage> {
         actions: [
           IconButton(
               onPressed: () {
-                nextScreen(context, GroupInfo(
-                  groupId: widget.groupId,
-                  groupName:  widget.groupName,
-                  adminName: admin,
-                ));
+                nextScreen(
+                    context,
+                    GroupInfo(
+                      groupId: widget.groupId,
+                      groupName: widget.groupName,
+                      adminName: admin,
+                    ));
               },
               icon: const Icon(Icons.info))
         ],
       ),
+      body: Stack(
+        children: <Widget>[
+          // Chat Messages Here
+          chatMessages(),
+          Container(
+            alignment: Alignment.bottomCenter,
+            width: MediaQuery.of(context).size.width,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
+              width: MediaQuery.of(context).size.width,
+              color: Colors.grey[700],
+              child: Row(children: [
+                Expanded(
+                    child: TextField(
+                  controller: messageController,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                    hintText: "Send a message...",
+                    hintStyle: TextStyle(color: Colors.white, fontSize: 16),
+                    border: InputBorder.none,
+                  ),
+                )),
+                const SizedBox(
+                  width: 12,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    sendMessage();
+                  },
+                  child: Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: const Center(
+                      child: Icon(
+                        Icons.send,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                )
+              ]),
+            ),
+          )
+        ],
+      ),
     );
+  }
+
+  chatMessages() {
+    return StreamBuilder(
+        stream: chats,
+        builder: (context, AsyncSnapshot snapshot) {
+          return snapshot.hasData
+              ? ListView.builder(
+                  itemCount: snapshot.data.docs.length,
+                  itemBuilder: (context, index) {
+                    return MessageTile(
+                        message: snapshot.data.docs[index]['message'],
+                        sender: snapshot.data.docs[index]['sender'],
+                        sentByMe: widget.userName ==
+                            snapshot.data.docs[index]['sender']);
+                  },
+                )
+              : Container();
+        });
+  }
+
+  sendMessage() {
+    if(messageController.text.isNotEmpty){
+      Map<String, dynamic> chatMessageMap = {
+        "message": messageController.text,
+        "sender": widget.userName,
+        "time": DateTime.now().millisecondsSinceEpoch,
+      };
+      DatasbaseService().sendMessage(widget.groupId, chatMessageMap);
+      setState(() {
+        messageController.clear();
+      });
+    }
   }
 }
